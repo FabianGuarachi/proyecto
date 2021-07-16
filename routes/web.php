03d81868cbf6ea7b23c5e7c8bf9e3a5b6c5aa73c
FabianGuarachi <lordxoca15@gmail.com>
2021-07-15 23:32:16 -0400

    nuevo

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-<<<<<<< Updated upstream
-    return "jeremy te voy a tunelear :v";
-=======
-    return "fabian culiao eri entero perkin larva y la conchetuamre";
->>>>>>> Stashed changes
+    return "hola";
 });
