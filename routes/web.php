1ba04b37a215c985b02109ed0793aa10303ef21e
FabianGuarachi <lordxoca15@gmail.com>
2021-07-19 22:03:24 -0400

    sss

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return "axel ql";
+    return view('plantilla');
+});
+Route::get('/plantilla', function () {
+    return view('plantilla');
 });
