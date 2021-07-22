2e4a4926387fbb74125cd638b3de74804572aac6
Axl <AxlBravo.A@gmail.com>
2021-07-22 16:40:06 -0400

    Cualqueir wea

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+//Agregar esta linea...use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //Agregar esta linea...Schema::defaultStringLength(191);
     }
 }
--- resources/views/Articulos/Index.blade.php
+++ resources/views/Articulos/Index.blade.php
@@ -1,3 +1,5 @@
+@extends("plantilla")
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
