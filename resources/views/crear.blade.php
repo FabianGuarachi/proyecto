53eac1d1b082019c27e0f77f898c68aaca84ec04
FabianGuarachi <lordxoca15@gmail.com>
2021-07-23 23:17:59 -0400

    hhhh

--- resources/views/crear.blade.php
+++ resources/views/crear.blade.php
@@ -1,24 +1,24 @@
 @extends('plantilla')
 
 @section('contenido')
-<h1> Formulario de creacion <h1>
+<h1> Formulario de creacion </h1>
 
-<div class="container mt-2">
-    <div class="row justify-content-center">
-        <div class="card">
-            <div class="card-header text-center"> Agregar Articulo</div>
-             <form>
-            
-             </form>
+                <form action="\articulos" method="POST">
+                    @csrf
+                    <div class="mt-3">
+                        <label class="form-label" for="">ID</label>
+                        <input type="text" class="form-control" id="id" name="id" tabindex="1"> 
+                    </div>
+                    <div class="mt-3">
+                        <label class="form-label" for="">DESCRIPCION</label>
+                        <textarea type="text" class="form-control" id="descripcion" name="descripcion" tabindex="2">  </textarea>                   
 
-
-
-
-
-        </div>
-
-    </div>
-</div>
+                    </div>
+                    <div class="mt-5">
+                        <a href="\articulos" class="btn btn-primary col-1">Cancelar</a>
+                        <button class="btn btn-success col-1" type="submyt">Crear</button>
+                    </div>
+                </form>
 
 
 @endsection
